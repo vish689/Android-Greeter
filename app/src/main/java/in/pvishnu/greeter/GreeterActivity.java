5497fd1d22957c957bfa5bd2efd1bc2ed2332d96
vish689 <vish689@gmail.com>
2016-03-19 14:45:42 +0530

    Final changes

--- app/src/main/java/in/pvishnu/greeter/GreeterActivity.java
+++ app/src/main/java/in/pvishnu/greeter/GreeterActivity.java
@@ -2,6 +2,7 @@ package in.pvishnu.greeter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class GreeterActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class GreeterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_greeter);
+
+        TextView textView = (TextView) findViewById(R.id.greeter_view);
+
+        textView.setText("Hello, " + getIntent().getStringExtra(MainActivity.GREETER_NAME_KEY));
     }
 }
--- app/src/main/java/in/pvishnu/greeter/MainActivity.java
+++ app/src/main/java/in/pvishnu/greeter/MainActivity.java
@@ -1,13 +1,25 @@
 package in.pvishnu.greeter;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String GREETER_NAME_KEY = "MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void greet(View view) {
+        EditText editText = (EditText) findViewById(R.id.edit_text);
+        String name = editText.getText().toString();
+        Intent intent = new Intent(this, GreeterActivity.class);
+        intent.putExtra(GREETER_NAME_KEY, name);
+        startActivity(intent);
     }
 }
